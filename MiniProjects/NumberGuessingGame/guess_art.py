efb0df3993bc7b3fe2c9b7692483bfdf7a4dd614
MohanSG <mohansg12@gmail.com>
2025-06-24 19:40:41 +0800

    finished numberguesser

---
+++ MiniProjects/NumberGuessingGame/guess_art.py
@@ -0,0 +1,25 @@
+art = '''
+ ,adPPYb,d8 88       88  ,adPPYba, ,adPPYba, ,adPPYba,  
+a8"    `Y88 88       88 a8P_____88 I8[    "" I8[    ""  
+8b       88 88       88 8PP"""""""  `"Y8ba,   `"Y8ba,   
+"8a,   ,d88 "8a,   ,a88 "8b,   ,aa aa    ]8I aa    ]8I  
+ `"YbbdP"Y8  `"YbbdP'Y8  `"Ybbd8"' `"YbbdP"' `"YbbdP"'  
+ aa,    ,88                                             
+  "Y8bbdP"                                              
+        88                      
+  ,d    88                      
+  88    88                      
+MM88MMM 88,dPPYba,   ,adPPYba,  
+  88    88P'    "8a a8P_____88  
+  88    88       88 8PP"""""""  
+  88,   88       88 "8b,   ,aa  
+  "Y888 88       88  `"Ybbd8"'  
+                                                   88  
+                                                   88  
+                                                   88  
+8b      db      d8  ,adPPYba,  8b,dPPYba,  ,adPPYb,88  
+`8b    d88b    d8' a8"     "8a 88P'   "Y8 a8"    `Y88  
+ `8b  d8'`8b  d8'  8b       d8 88         8b       88  
+  `8bd8'  `8bd8'   "8a,   ,a8" 88         "8a,   ,d88  
+    YP      YP      `"YbbdP"'  88          `"8bbdP"Y8  
+'''
---
+++ MiniProjects/NumberGuessingGame/numberguesser.py
@@ -0,0 +1,43 @@
+import random
+import os
+from guess_art import art
+
+random_number = random.randint(1, 100)
+guess_count = 0
+
+os.system('cls')
+
+def check(num, answer):
+    if num > answer:
+        print("Too high")
+        return False
+    elif num < answer:
+        print("Too low")
+        return False
+    else:
+        return True
+
+print(art)
+print("Welcome to the Number Guessing Game!")
+difficulty = input("Choose a difficulty. 'easy' or 'hard: ")
+
+if difficulty == "hard":
+    guess_count = 5
+elif difficulty == "easy":
+    guess_count = 10
+
+in_play = True
+while in_play:
+
+    print(f"You have {guess_count} attempts to guess the number")
+
+    guess = int(input("Make a guess: "))
+    if check(guess, random_number):
+        print(f"Congratulations! You guessed the number {random_number} in {10 - guess_count} guesses!")
+        in_play = False
+    else:
+        guess_count -= 1
+
+    if guess_count < 1:
+        print("You have run out of lives!")
+        in_play = False
