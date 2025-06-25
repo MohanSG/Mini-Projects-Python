37301c39a4a5a67f1a81cf5b1ad4ae724bfcc91c
MohanSG <mohansg12@gmail.com>
2025-06-25 16:38:21 +0800

    update main.py

--- MiniProjects/HigherOrLower/main.py
+++ MiniProjects/HigherOrLower/main.py
@@ -3,7 +3,7 @@ from gameart import art
 import random
 import os
 
-def get_random_insta():
+def get_random_insta(data):
     return data[random.randint(0, len(data) - 1)]
 
 def check_choice(player_choice, other_choice):
@@ -12,13 +12,14 @@ def check_choice(player_choice, other_choice):
     elif other_choice > player_choice:
         return False
     
-card_a = get_random_insta()
-card_b = get_random_insta()
+card_a = get_random_insta(data)
+card_b = get_random_insta(data)
 
 os.system('cls')
 print(art)
+
 print(f"Compare A: {card_a['name']}, a {card_a['description']}, from {card_a['country']}")
-
 print('vs.')
-
 print(f"Compare A: {card_b['name']}, a {card_b['description']}, from {card_b['country']}")
+
+player_choice = input('Who has more followers? Type "A" or "B": ')
