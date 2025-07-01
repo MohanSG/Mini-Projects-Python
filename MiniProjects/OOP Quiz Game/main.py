41f0114148487f98843de416ce91312584cdda98
MohanSG <mohansg12@gmail.com>
2025-07-01 19:58:49 +0800

    finished quiz game

---
+++ MiniProjects/OOP Quiz Game/main.py
@@ -0,0 +1,14 @@
+from question_model import Question
+from data import question_data
+from quiz_brain import QuizBrain
+
+question_bank = []
+for question in question_data:
+    question_bank.append(Question(question['question'], question['correct_answer']))
+
+quiz_brain = QuizBrain(question_bank)
+
+while quiz_brain.still_has_questions():
+    quiz_brain.next_question()
+
+print(f"You've completed the quiz!\nYour final score is {quiz_brain.score}/{quiz_brain.question_number}")
