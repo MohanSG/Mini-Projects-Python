e1855bd0ec2485c01857cffef1a0c6107dde8334
MohanSG <mohansg12@gmail.com>
2025-07-01 14:26:36 +0800

    completed oop coffee machine

--- MiniProjects/OOP Coffee Machine/main.py
+++ MiniProjects/OOP Coffee Machine/main.py
@@ -1,3 +1,30 @@
 from menu import Menu
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
+
+Coffee_Maker = CoffeeMaker()
+Coffee_Menu = Menu()
+Money_Machine = MoneyMachine()
+
+operating = True
+while operating: 
+    choice = input(f"What would you like? ({Coffee_Menu.get_items()}): ")
+
+    if choice == "report":
+        Coffee_Maker.report()
+        Money_Machine.report()
+        
+    elif choice == "off":
+        operating = False
+        print("Shutting down....")
+        
+    elif choice == "espresso" or choice == "latte" or choice =="cappuccino":
+        chosen_coffee = Coffee_Menu.find_drink(choice)
+        
+        if Coffee_Maker.is_resource_sufficient(chosen_coffee):
+            
+            if Money_Machine.make_payment(chosen_coffee.cost):
+                Coffee_Maker.make_coffee(chosen_coffee)
+                
+        else:
+            print("Not enough resources. Sorry!")
