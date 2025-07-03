fceb7c8315d29b115f75672e86b5ea5a036dba04
MohanSG <mohansg12@gmail.com>
2025-07-03 20:03:33 +0800

    started snake game, finished turtle race

---
+++ MiniProjects/Snake Game/main.py
@@ -0,0 +1,33 @@
+from turtle import Turtle, Screen
+
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.bgcolor("black")
+screen.title("My Snake Game")
+screen.tracer(1)
+
+snake_bits = []
+snake_length_unit = 0
+
+x = 0
+y = 0
+
+for _ in range(3):
+    t = Turtle("square")
+    t.penup()
+    t.color("white")
+    t.goto(x, y)
+    x -= 20
+    snake_bits.append(t)
+
+is_playing = True
+
+while is_playing:
+    for bit in snake_bits:
+        bit.forward(20)
+        screen.update()
+
+        if bit.xcor() > 570:
+            is_playing = False
+            
+screen.exitonclick()
--- MiniProjects/Turtle Race/main.py
+++ MiniProjects/Turtle Race/main.py
@@ -1,18 +1,36 @@
 from turtle import Turtle, Screen
+import random
 
 turtles = []
 screen = Screen()
-screen.screensize(500, 500)
+colors = ("blue", "green", "yellow", "orange", "red", "purple")
 
+is_race_on = False
+screen.setup(width=500, height=500)
 
 for t in range(6):
     turtles.append(Turtle(shape="turtle"))
 
 y = 190
-for turtle in turtles:
-    turtle.penup()
-    turtle.goto(-248, y)
+for i in range(len(turtles)):
+    turtles[i].color(colors[i])
+    turtles[i].penup()
+    turtles[i].goto(-230, y)
 
     y -= 75
+
+choice = screen.textinput("Turtle Race", "Place your bet!")
+is_race_on = True
+
+while(is_race_on):
+    for turtle in turtles:
+        turtle.forward(random.randint(1, 10))
+
+        if turtle.xcor() > 225:
+            if turtle.pencolor() == choice:
+                print(f"The {turtle.pencolor()} turtle won! You got it right!")
+            else:
+                print(f"The {turtle.pencolor()} turtle won! You got it wrong!")
+            is_race_on = False
 
 screen.mainloop()
