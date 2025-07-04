2c3d71bcd41c73c4250cc3d15808c046816a4582
MohanSG <mohansg12@gmail.com>
2025-07-04 16:16:55 +0800

    updated snake game

--- MiniProjects/Snake Game/main.py
+++ MiniProjects/Snake Game/main.py
@@ -1,10 +1,11 @@
 from turtle import Turtle, Screen
+import time
 
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.bgcolor("black")
 screen.title("My Snake Game")
-screen.tracer(1)
+screen.tracer(0)
 
 snake_bits = []
 snake_length_unit = 0
@@ -20,14 +21,17 @@ for _ in range(3):
     x -= 20
     snake_bits.append(t)
 
+
 is_playing = True
 
 while is_playing:
     for bit in snake_bits:
         bit.forward(20)
-        screen.update()
 
         if bit.xcor() > 570:
             is_playing = False
             
+    screen.update()
+    time.sleep(1)
+        
 screen.exitonclick()
