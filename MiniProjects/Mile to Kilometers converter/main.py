d828653e9fa600b2af9590afc39beb1fac683527
MohanSG <mohansg12@gmail.com>
2025-07-18 16:01:00 +0800

    progress on m to km project

---
+++ MiniProjects/Mile to Kilometers converter/main.py
@@ -0,0 +1,16 @@
+import tkinter
+
+window = tkinter.Tk()
+window.title("Mile to Kilometers Converter")
+window.minsize(width=500, height=300)
+
+tkinter.Label(text="Mile to Kilometers Converter").pack()
+
+
+
+
+
+
+
+
+window.mainloop()
