07c666e265cb325de23093d5a9c87f744aa1a5d6
MohanSG <mohansg12@gmail.com>
2025-08-14 16:39:07 +0800

    grabbing uris from spotify api complete

--- MiniProjects/MusicalTimeMachine/main.py
+++ MiniProjects/MusicalTimeMachine/main.py
@@ -2,10 +2,33 @@ import requests
 from bs4 import BeautifulSoup
 from datetime import datetime
 import os
+from dotenv import load_dotenv
+import spotipy
+from spotipy.oauth2 import SpotifyOAuth
+from pprint import pprint
 
+load_dotenv()
 expected_format = '%Y-%m-%d'
+year = expected_format.find('%Y')
 is_incorrect_format = True
 date = ""
+
+def get_song_uris(name, artist):
+    song_uris = []
+    if len(name) == len(artist):
+        for index in range(len(name)):
+            track = name[index].get_text(strip=True)
+            results = sp.search(
+                q=f"track:{track} artist:{artist[index]}", limit=1)
+
+            if results['tracks']['items']:
+                song_uris.append(results['tracks']['items'][0]['uri'])
+                print(results['tracks']['items'][0]['uri'])
+            else:
+                print(f"Song:{track} by {artist[index]} could not be found. Skipping...")
+
+
+    return song_uris
 
 while is_incorrect_format:
     date = input("Which year do you want to travel to? Type the date in this format YYYY-MM-DD:\n")
@@ -24,12 +47,19 @@ headers = {
 response = requests.get(URL, headers=headers)
 soup = BeautifulSoup(response.text, 'html.parser')
 
-chart_results = soup.find_all(name="li", class_="o-chart-results-list__item")
-for results in chart_results:
-    if results is not None:
-        song_title = results.find("h3")
-        if song_title is not None:
-            print(song_title.get_text(strip=True))
+song_names = soup.select("li ul li h3")
+song_artists = [item.next_sibling.next_sibling.get_text(strip=True) for item in song_names]
 
+print(f"{len(song_names)}")
+print(f"{len(song_artists)}")
 
+sp = spotipy.Spotify(auth_manager=SpotifyOAuth(
+    client_id=os.environ["SPOTIFY_CLIENT_ID"],
+    client_secret=os.environ["SPOTIFY_CLIENT_SECRET"],
+    redirect_uri=os.environ["SPOTIFY_REDIRECT_URI"],
+    scope="user-library-read",
+))
 
+uris = get_song_uris(song_names, song_artists)
+# results = sp.search(q=f"track:{song_names[4].get_text(strip=False)} artist:{song_artists[4]}")
+# print(results)
