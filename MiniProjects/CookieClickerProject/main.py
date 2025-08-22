8484afdc20be6d0e7aa09ea58886fc5a135174d4
MohanSG <mohansg12@gmail.com>
2025-08-22 16:41:49 +0800

    fixed automatic upgrades

--- MiniProjects/CookieClickerProject/main.py
+++ MiniProjects/CookieClickerProject/main.py
@@ -22,8 +22,24 @@ def choose_product(number_of_cookies): #Loop through products and choose the mos
 
     return None
 
+def choose_upgrade():
+    try:
+        cookie_upgrades = driver.find_element(By.ID, "upgrades")
+        cookie = cookie_upgrades.find_element(By.ID, "upgrade0")
+    except Exception:
+        print("No upgrades available")
+        return None
+    else:
+        return cookie
+
+def change_number_format(number_text):
+    if ',' in number_text:
+        new_number = int(number_text.replace(",", ""))
+    else:
+        new_number = int(cookie_amount_str)
+    return new_number
+
 cookie_amount = 0
-cursor = 0
 
 time.sleep(5)#Wait for js message to go away
 big_cookie = get_big_cookie()
@@ -35,20 +51,22 @@ while cookie_amount < 1000000:
     while time.time() < timeout_start + timeout:
         big_cookie.click()
 
+    upgrade = choose_upgrade()
+    if upgrade is not None:
+        print("Buying upgrade")
+        upgrade.click()
+
     cookie_amount_info = driver.find_element(By.ID, "cookies")
     cookie_amount_str = cookie_amount_info.text.split(" ")[0]
     cookies_per_second = cookie_amount_info.text.split("\n")[1].split(" : ")[1]
     print(f"cookies/second {cookies_per_second}")
 
-    if ',' in cookie_amount_str:
-        cookie_amount = int(cookie_amount_str.replace(",", ""))
-    else:
-        cookie_amount = int(cookie_amount_str)
+    cookie_amount_int = change_number_format(cookie_amount_str)
 
-    cursor = choose_product(cookie_amount)
-    if cursor is not None:
-        if int(cursor.text) < cookie_amount:
-            cursor.click()
-            time.sleep(1)
+    product = choose_product(cookie_amount_int)
+    if product is not None:
+        product_number = change_number_format(product.text)
+        product.click()
+        time.sleep(1)
     else:
         print("Nothing to buy")
